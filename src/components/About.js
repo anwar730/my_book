641ee434448aa45a611e34c42d6869c60470868c
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-10-05 18:17:48 +0300

    Style the about componnt

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Subscription from './Subscription'
+import Footer from './Footer'
 
 function About() {
   return (
     <div>
-      <p>About</p>
+      <div className='about-content'>
+      <h3>About Us ..</h3>
+      <p>Book Shelf is the world's largest site for readers <br/>
+      and book recommendations.Our mission is to <br/>
+      help people find and share books they love.<br/>
+      Book Shelf was launched in January 2007.
+      </p>
+      </div>
+
+      
+      <Footer/>
     </div>
   )
 }
