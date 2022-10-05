b137c1a457cc236e272eedf4f09ede304827bc7c
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-10-05 15:48:16 +0300

    Render the books on the page

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,8 +6,18 @@ function Book({image,title,desription,year,author}) {
     // console.log (year)
     
   return (
+    
     <>
+      <div className='card'>
+      <img src={image} alt="Book Image"/>
+      <h3>{title}</h3>
+      <h5>{author}</h5>
+      <p>{year}</p>
+      <button>Read</button>
+      </div>
     </>
+
+    
   )
   
 }
