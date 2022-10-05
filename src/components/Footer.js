ba235e73291f44ddb48312301c99fc4ad8267abe
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-10-05 17:54:47 +0300

    Create and style the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,31 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      
+    <div id="container">
+        <div id='sm-cont'>
+      <div id ='policies'>
+        <h3>Our Policies</h3>
+        <p>FAQ</p>
+        <p>Privacy Policy</p>
+        <p>Cookie Policy</p>
+        <p>Terms and conditions</p>
+        
+        
+      </div>
+      <div id='About us'>
+        <h3>About Us</h3>
+        <p>Our Company</p>
+        <p>Our History</p>
+        <p>Contact</p>
+        <p>Journal</p>
+        <p>Store Locations</p>
+      </div>
+      <div>
+        <h3>Follow Us</h3>
+      </div>
+      </div>
+      <p id='done'>All Rights Reserved By Anwar</p>
+
     </div>
   )
 }
